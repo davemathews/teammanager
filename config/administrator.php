c12df6671d0c17d9e6715a7bb40efc33430a0c09
David Mathews <david.mathews@spinen.com>
2015-12-25 14:15:06 -0500

    adding auth to admin

--- config/administrator.php
+++ config/administrator.php
@@ -68,8 +68,7 @@ return array(
 	 */
 	'permission'=> function()
 	{
-		//return Auth::check();
-		return true;
+		return Auth::check();
 	},
 
 	/**
@@ -106,14 +105,14 @@ return array(
 	 *
 	 * @type string
 	 */
-	'login_path' => 'auth/login',
+	'login_path' => 'login',
 
 	/**
 	 * The logout path is the path where Administrator will send the user when they click the logout link
 	 *
 	 * @type string
 	 */
-	'logout_path' => false,
+	'logout_path' => 'logout',
 
 	/**
 	 * This is the key of the return path that is sent with the redirection to your login_action. Session::get('redirect') will hold the return URL.
