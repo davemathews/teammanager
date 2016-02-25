1db2b155105febca038cb2e138d5c3f9a7f7f43b
David Mathews <david.mathews@spinen.com>
2016-02-25 08:53:22 -0500

    adding the recruits to the admin

---
+++ config/administrator/recruit_notes.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Recruit Notes  model config
+ */
+
+return array(
+
+    'title' => 'Recruit Notes',
+
+    'single' => 'Recruit Note',
+
+    'model' => 'App\RecruitNote',
+
+    /**
+     * The sort options for a model
+     *
+     * @type array
+     */
+    'sort' => array(
+        'field' => 'contact_date',
+        'direction' => 'asc',
+    ),
+
+    /**
+     * The display columns
+     */
+    'columns' => array(
+        'recruit' => array(
+            'title' => 'Recruit',
+            'relationship' => 'recruits',
+            'select' => "Concact((:table).first_name, ' ', (:table).last_name)",
+        ),
+        'contact_date' => array(
+            'title' => 'Contact Date',
+        ),
+        'contacted_how' => array(
+            'title' => 'Contacted How',
+        ),
+        'note' => array(
+            'title' => 'Note',
+        ),
+    ),
+
+    /**
+     * The filter set
+     */
+    'filters' => array(
+        'recruit' => array(
+            'title' => 'Recruit',
+            'type' => 'relationship',
+            'name_field' => 'last_name',
+        ),
+        'contact_date' => array(
+            'title' => 'Contact Date',
+            'type' => 'date',
+        ),
+        'contacted_how' => array(
+            'title' => 'Contacted How',
+            'type' => 'text',
+        ),
+        'note' => array(
+            'title' => 'Note',
+            'type' => 'text',
+        ),
+    ),
+
+    /**
+     * The editable fields
+     */
+    'edit_fields' => array(
+        'recruit' => array(
+            'title' => 'Recruit',
+            'type' => 'relationship',
+            'name_field' => 'last_name',
+        ),
+        'contact_date' => array(
+            'title' => 'Contact Date',
+            'type' => 'date',
+        ),
+        'contacted_how' => array(
+            'title' => 'Contacted How',
+            'type' => 'text',
+        ),
+        'note' => array(
+            'title' => 'Note',
+            'type' => 'text',
+        ),
+    ),
+);
+
---
+++ config/administrator/recruits.php
@@ -0,0 +1,300 @@
+<?php
+
+/**
+ * Recruits  model config
+ */
+
+return array(
+
+    'title' => 'Recruits',
+
+    'single' => 'Recruit',
+
+    'model' => 'App\Recruit',
+
+    /**
+     * The sort options for a model
+     *
+     * @type array
+     */
+    'sort' => array(
+        'field' => 'last_name',
+        'direction' => 'asc',
+    ),
+
+    /**
+     * The display columns
+     */
+    'columns' => array(
+        'first_name' => array(
+            'title' => 'First Name',
+        ),
+        'last_name' => array(
+            'title' => 'Last Name',
+        ),
+        'email' => array(
+            'title' => 'Email',
+        ),
+        'phone' => array(
+            'title' => 'Phone',
+        ),
+        'city' => array(
+            'title' => 'City',
+        ),
+        'state' => array(
+            'title' => 'State',
+        ),
+        'weight' => array(
+            'title' => 'Weight',
+        ),
+        'age' => array(
+            'title' => 'Age',
+        ),
+        'dob' => array(
+            'title' => 'DOB',
+        ),
+        'high_school' => array(
+            'title' => 'High School',
+        ),
+        'high_school_graduation_year' => array(
+            'title' => 'High School Grad',
+        ),
+        'gpa' => array(
+            'title' => 'GPA',
+        ),
+        'act' => array(
+            'title' => 'ACT',
+        ),
+        'sat' => array(
+            'title' => 'SAT',
+        ),
+        'reading_taken' => array(
+            'title' => 'Reading Taken',
+        ),
+        'intended_major' => array(
+            'title' => 'Intended Major',
+        ),
+        'signed' => array(
+            'title' => 'Signed',
+        ),
+        'signed_date' => array(
+            'title' => 'Signed Date',
+        ),
+        'offer' => array(
+            'title' => 'Offer',
+        ),
+        'declined' => array(
+            'title' => 'Declined',
+        ),
+        'declined_date' => array(
+            'title' => 'Declined Date',
+        ),
+        'school_declined_for' => array(
+            'title' => 'School Declined For',
+        ),
+        'rosters' => array(
+            'title' => 'Roster',
+            'relationship' => 'rosters',
+            'select' => "(:table).name",
+        ),
+    ),
+
+    /**
+     * The filter set
+     */
+    'filters' => array(
+        'first_name' => array(
+            'title' => 'First Name',
+            'type' => 'text',
+        ),
+        'last_name' => array(
+            'title' => 'Last Name',
+            'type' => 'text',
+        ),
+        'email' => array(
+            'title' => 'Email',
+            'type' => 'text',
+        ),
+        'phone' => array(
+            'title' => 'Phone',
+            'type' => 'text',
+        ),
+        'city' => array(
+            'title' => 'City',
+            'type' => 'text',
+        ),
+        'state' => array(
+            'title' => 'State',
+            'type' => 'text',
+        ),
+        'weight' => array(
+            'title' => 'Weight',
+            'type' => 'number',
+        ),
+        'age' => array(
+            'title' => 'Age',
+            'type' => 'number',
+        ),
+        'dob' => array(
+            'title' => 'DOB',
+            'type' => 'date',
+        ),
+        'high_school' => array(
+            'title' => 'High School',
+            'type' => 'text',
+        ),
+        'high_school_graduation_year' => array(
+            'title' => 'High School Grad',
+            'type' => 'number',
+        ),
+        'gpa' => array(
+            'title' => 'GPA',
+            'type' => 'number',
+        ),
+        'act' => array(
+            'title' => 'ACT',
+            'type' => 'number',
+        ),
+        'sat' => array(
+            'title' => 'SAT',
+            'type' => 'number',
+        ),
+        'reading_taken' => array(
+            'title' => 'Reading Taken',
+            'type' => 'bool',
+        ),
+        'intended_major' => array(
+            'title' => 'Intended Major',
+            'type' => 'text',
+        ),
+        'signed' => array(
+            'title' => 'Signed',
+            'type' => 'bool',
+        ),
+        'signed_date' => array(
+            'title' => 'Signed Date',
+            'type' => 'date',
+        ),
+        'offer' => array(
+            'title' => 'Offer',
+            'type' => 'number',
+        ),
+        'declined' => array(
+            'title' => 'Declined',
+            'type' => 'bool',
+        ),
+        'declined_date' => array(
+            'title' => 'Declined Date',
+            'type' => 'date',
+        ),
+        'school_declined_for' => array(
+            'title' => 'School Declined For',
+            'type' => 'text',
+        ),
+        'rosters' => array(
+            'title' => 'Roster',
+            'type' => 'relationship',
+            'name_field' => 'name',
+        ),
+    ),
+
+    /**
+     * The editable fields
+     */
+    'edit_fields' => array(
+        'first_name' => array(
+            'title' => 'First Name',
+            'type' => 'text',
+        ),
+        'last_name' => array(
+            'title' => 'Last Name',
+            'type' => 'text',
+        ),
+        'email' => array(
+            'title' => 'Email',
+            'type' => 'text',
+        ),
+        'phone' => array(
+            'title' => 'Phone',
+            'type' => 'text',
+        ),
+        'city' => array(
+            'title' => 'City',
+            'type' => 'text',
+        ),
+        'state' => array(
+            'title' => 'State',
+            'type' => 'text',
+        ),
+        'weight' => array(
+            'title' => 'Weight',
+            'type' => 'number',
+        ),
+        'age' => array(
+            'title' => 'Age',
+            'type' => 'number',
+        ),
+        'dob' => array(
+            'title' => 'DOB',
+            'type' => 'date',
+        ),
+        'high_school' => array(
+            'title' => 'High School',
+            'type' => 'text',
+        ),
+        'high_school_graduation_year' => array(
+            'title' => 'High School Grad',
+            'type' => 'number',
+        ),
+        'gpa' => array(
+            'title' => 'GPA',
+            'type' => 'number',
+        ),
+        'act' => array(
+            'title' => 'ACT',
+            'type' => 'number',
+        ),
+        'sat' => array(
+            'title' => 'SAT',
+            'type' => 'number',
+        ),
+        'reading_taken' => array(
+            'title' => 'Reading Taken',
+            'type' => 'bool',
+        ),
+        'intended_major' => array(
+            'title' => 'Intended Major',
+            'type' => 'text',
+        ),
+        'signed' => array(
+            'title' => 'Signed',
+            'type' => 'bool',
+        ),
+        'signed_date' => array(
+            'title' => 'Signed Date',
+            'type' => 'date',
+        ),
+        'offer' => array(
+            'title' => 'Offer',
+            'type' => 'number',
+        ),
+        'declined' => array(
+            'title' => 'Declined',
+            'type' => 'bool',
+        ),
+        'declined_date' => array(
+            'title' => 'Declined Date',
+            'type' => 'date',
+        ),
+        'school_declined_for' => array(
+            'title' => 'School Declined For',
+            'type' => 'text',
+        ),
+        'rosters' => array(
+            'title' => 'Roster',
+            'type' => 'relationship',
+            'name_field' => 'name',
+        ),
+    ),
+);
+
