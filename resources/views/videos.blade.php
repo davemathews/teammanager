fe2686d98138405a7f56081910cfaf4b667f8606
David Mathews <david.mathews@spinen.com>
2016-03-01 10:34:56 -0500

    adding new videos page

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,5 +1,9 @@
 <?php
 //Show first page of results, Set the number of items to show on each page to 50, Sort by relevance, Show results in descending order, and Filter only Creative Commons License videos
-$search_results = Vimeo::request('/me/videos', array('page' => 1, 'per_page' => 10));
-print_r($search_results);
+$searchResults = Vimeo::request('/me/videos', array('page' => 1, 'per_page' => 10));
+print_r($searchResults);
 ?>
+@foreach($searchResults['data'] as $videoData)
+    <h2>{{ $videoData['name'] }}</h2>
+    {{ $videoData['embed']['html'] }}
+@endforeach
