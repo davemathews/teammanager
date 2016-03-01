0cc309dafd14bc8e7dfdca286304442651d7870a
David Mathews <david.mathews@spinen.com>
2016-03-01 13:53:15 -0500

    fixing configs

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -2,7 +2,7 @@
 <?php
 //Show first page of results, Set the number of items to show on each page to 50, Sort by relevance, Show results in descending order, and Filter only Creative Commons License videos
 $searchResults = Vimeo::request('/me/videos', array('page' => 1, 'per_page' => 10));
-//print_r($searchResults);
+print_r($searchResults);
 ?>
 @foreach($searchResults['body']['data'] as $videoData)
     <h2>{{ $videoData['name'] }}</h2>
