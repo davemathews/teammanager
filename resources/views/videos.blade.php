c02366dadec7674d22c57902be4ced300d31407b
David Mathews <david.mathews@spinen.com>
2016-03-01 20:40:38 -0500

    fixing configs

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,10 +1,16 @@
 <div style="margin-top: 35px;">
 <?php
 //Show first page of results, Set the number of items to show on each page to 50, Sort by relevance, Show results in descending order, and Filter only Creative Commons License videos
-$searchResults = Vimeo::request('/me/videos', array('page' => 1, 'per_page' => 12));
+    $page = 1;
+    if($_GET['page']){
+        $page = $_GET['page'];
+    }
+    $searchResults = Vimeo::request('/me/videos', array('page' => $page, 'per_page' => 12));
 //print_r($searchResults);
 ?>
+    <div style="display:block;">
     <a href="@if($searchResults['body']['paging']['previous']) {{ "?page=".($searchResults['body']['page'] - 1) }} @endif"><-Prev</a> <a href="@if($searchResults['body']['paging']['next']) {{ "?page=".($searchResults['body']['page'] + 1) }} @endif">Next-></a>
+    </div>
 @foreach($searchResults['body']['data'] as $videoData)
     <div style="display:inline-block;">
         <iframe src="https://player.vimeo.com/{{ preg_replace("/\/videos/","video",$videoData['uri']) }}?badge=0&autopause=0&player_id=0" width="512" height="288" frameborder="0" title="{{ $videoData['name'] }}" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
