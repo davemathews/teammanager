6697ca46097d7996d72861049a6029851a2c9454
David Mathews <david.mathews@spinen.com>
2016-03-01 20:30:46 -0500

    fixing configs

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -4,7 +4,7 @@
 $searchResults = Vimeo::request('/me/videos', array('page' => 1, 'per_page' => 12));
 print_r($searchResults);
 ?>
-    <a href="@if($searchResults['paging']['previous']) {{ "?page=".($searchResults['page'] - 1) }} @endif"><-Prev</a> <a href="@if($searchResults['paging']['next']) {{ "?page=".($searchResults['page'] + 1) }} @endif">Next-></a>
+    <a href="@if($searchResults['body']['paging']['previous']) {{ "?page=".($searchResults['body']['page'] - 1) }} @endif"><-Prev</a> <a href="@if($searchResults['body']['paging']['next']) {{ "?page=".($searchResults['body']['page'] + 1) }} @endif">Next-></a>
 @foreach($searchResults['body']['data'] as $videoData)
     <div style="display:inline-block;">
         <iframe src="https://player.vimeo.com/{{ preg_replace("/\/videos/","video",$videoData['uri']) }}?badge=0&autopause=0&player_id=0" width="512" height="288" frameborder="0" title="{{ $videoData['name'] }}" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
